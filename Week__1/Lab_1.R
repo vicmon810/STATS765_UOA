a5e17a184e651448166c11b7a13b57a2617d1c30
vicmon810 <maokr879@student.otago.ac.nz>
2024-02-26 16:06:47 +1300

    Week 1 lab work

--- Week__1/Lab_1.R
+++ Week__1/Lab_1.R
@@ -1,3 +1,6 @@
+#install tidyverse
+install.packages("tidyverse")
+
 # load library
 library(tidyverse)
 
@@ -18,3 +21,17 @@ colnames(alarms_count.mt) <- alarms_dates.vt
 # set up data frames
 alarms_count.df <- data.frame(alarm_id = alarms_id.vt, alarms_count.mt)
 alarms_info.df <- data.frame(alarm_ID = alarms_id.vt, alarms_region = alarms_region.vt)
+
+alarms_count_long.df <- alarms_count.df  %>%  pivot_longer(cols = !alarm_id, names_to = 'date', values_to= 'frequency')
+#print(alarms_count_long.df)
+alarms_count_long.df <- alarms_count_long.df %>% separate(date, c('prefix',"year",'month','day'))
+#print(alarms_count_long.df)
+alarm_count_sa.df <- alarms_count_long.df %>% select(-prefix) %>% mutate(catrgory = if_else(frequency>=400, 'high', 'low') )
+#print(alarm_count_sa.df)
+
+alarm_merge.df <- alarm_count_sa.df %>% left_join(alarms_info.df, by =  c("alarm_id" = "alarm_ID"))     
+#print(alarm_merge.df)
+
+alarm_sum.df <- alarm_merge.df %>% group_by(year,month,day, alarms_region) %>% summarise(total = sum(frequency), percent_high = 100 * sum(catrgory == "high") / n())
+print(alarm_sum.df)
+
