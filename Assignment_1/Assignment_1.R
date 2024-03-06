00003eb4f236af9d78e9603014742c2f1b15ef85
Kris Mao <vicmon@wifi-172-23-48-28.uoa-wifi.auckland.ac.nz>
2024-03-07 08:49:58 +1300

    Assignemtn 1 updating

--- Assignment_1/Assignment_1.R
+++ Assignment_1/Assignment_1.R
@@ -1,1 +1,28 @@
-data <- read.csv(file.choose())
+# import needed library 
+library(tidyverse)
+# read data from folder
+data <- read.csv("/Users/vicmon/stat765/STATS765_UOA/Assignment_1/rent.csv")
+
+#Time 
+time <- data[,0]
+#location Id
+location_id <- data[,1]
+#lodged bonds 
+lodged_bonds <- data[,4]
+# active bonds 
+active_bonds <- data[,5]
+
+lodged_bonds%>%ggplot(aes(time))+geom_bar()
+
+# Auckland 
+# Wellington
+# Christchurch 
+# Hamilton
+
+
+
+
+
+
+
+
